77e17d91c9c38b297a91a7ad4a2994f1ae3e3852
Montadhar-Al-Daraji <140853760+Montadhar-Al-Daraji@users.noreply.github.com>
2025-09-16 02:53:33 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,7 @@
+// ملف config.js
+// هذا الملف يحتوي على المفاتيح السرية ويجب عدم رفعه إلى GitHub
+
 window.CONFIG = {
-  SUPABASE_URL: 'your_actual_supabase_url',
-  SUPABASE_ANON_KEY: 'your_actual_supabase_anon_key'
+  SUPABASE_URL: 'https://xzltdsmmolyvcmkfzedf.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6bHRkc21tb2x5dmNta2Z6ZWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2Nzg1NzEsImV4cCI6MjA3MzI1NDU3MX0.3TJ49ctEhOT1KDIFtZXFw2jwTq57ujaWbqNNJ2Eeb1U'
 };
