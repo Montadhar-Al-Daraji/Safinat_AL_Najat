1397b94479f30a9f22e611b3da5ebb6306e0f364
Montadhar-Al-Daraji <140853760+Montadhar-Al-Daraji@users.noreply.github.com>
2025-09-18 00:54:09 +0300

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,4 +1,4 @@
-// admin/config.js
+
 const CONFIG = {
     SUPABASE_URL: 'https://xzltdsmmolyvcmkfzedf.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6bHRkc21tb2x5dmNta2Z6ZWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2Nzg1NzEsImV4cCI6MjA3MzI1NDU3MX0.3TJ49ctEhOT1KDIFtZXFw2jwTq57ujaWbqNNJ2Eeb1U',
